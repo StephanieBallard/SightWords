39348dcd889e32fd71db73529a13fc1aaaffb876
Stephanie <user.name>
2021-01-25 00:44:01 -0800

    words left functionality implemented

--- SightWords/ViewController/SightWordsViewController.swift
+++ SightWords/ViewController/SightWordsViewController.swift
@@ -14,7 +14,7 @@ class SightWordsViewController: UIViewController {
     var index = 0
     
     // MARK: - Outlets -
-    @IBOutlet weak var cardCountTextLabel: UILabel!
+    @IBOutlet weak var wordCountTextLabel: UILabel!
     @IBOutlet weak var wordTextLabel: UILabel!
     @IBOutlet weak var nextButton: UIButton!
     
@@ -27,7 +27,7 @@ class SightWordsViewController: UIViewController {
     // MARK: - Actions -
     @IBAction func shuffleButtonTapped(_ sender: UIButton) {
         words.shuffle()
-        print(words)
+//        print(words)
     }
     
     @IBAction func nextButtonTapped(_ sender: UIButton) {
@@ -39,12 +39,14 @@ class SightWordsViewController: UIViewController {
             index += 1
         }
         wordTextLabel.text = words[index]
+        wordCountTextLabel.text = "Words Left: \(index + 1) / 133"
     }
     
     // MARK: - Helper Functions -
     func configureUI() {
         view.backgroundColor = #colorLiteral(red: 0.6196078431, green: 0.9647058824, blue: 1, alpha: 1)
         wordTextLabel.text = words[0]
+        wordCountTextLabel.text = "Words Left: \(index + 1) / 133"
         configureNextButton()
     }
     
@@ -52,14 +54,18 @@ class SightWordsViewController: UIViewController {
         let alert = UIAlertController(title: "Congratulations!", message: "You Did It!", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Practice again?", style: .default, handler: nil))
         present(alert, animated: true)
+        words.shuffle()
     }
     
     func configureNextButton() {
-        nextButton.layer.cornerRadius = 12
+        nextButton.setDimensions(height: 50, width: 150)
+        nextButton.setTitle("Next", for: .normal)
+        nextButton.titleLabel?.font = UIFont(name: "Noto Sans Myanmar Bold", size: 26.0)
+        nextButton.layer.cornerRadius = 12.0
         nextButton.layer.borderWidth = 2.0
         nextButton.layer.borderColor = UIColor.white.cgColor
         nextButton.layer.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         nextButton.setTitleColor(#colorLiteral(red: 0.5490196078, green: 0.5058823529, blue: 0.9647058824, alpha: 1), for: .normal)
-        nextButton.setDimensions(height: 50, width: 100)
+        nextButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline).withSize(22)
     }
 }
